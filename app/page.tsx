3082ab0c3867f2b14879c160b32cce664a989c78
Atridad Lahiji <me@atri.dad>
2023-09-16 16:45:49 -0600

    Added new CSS and made basic homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,23 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="">Homepage</h1>
+    <main className="flex flex-col text-center items-center justify-center px-4 py-16 gap-4">
+      <h1 className="text-3xl sm:text-6xl font-bold">
+        <span className="bg-gradient-to-br from-blue-600 to-green-400 bg-clip-text text-transparent box-decoration-clone">
+          Skip
+        </span>
+        Ahead
+      </h1>
+
+      <h2 className="text-xl">SOMETHING HERE</h2>
+
+      <div className="divider"></div>
+
+      <p className="text-xl">Enter:</p>
+
+      <div className="flex flex-row flex-wrap gap-4">
+        <button className="btn btn-outline btn-primary">Vendor</button>
+        <button className="btn btn-outline btn-secondary">Customer</button>
+      </div>
     </main>
   );
 }
