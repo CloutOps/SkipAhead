823254ef9630057f42ea8cdb1d324102316615bb
Atridad Lahiji <me@atri.dad>
2023-09-16 16:01:56 -0600

    DaisyUI

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Hello world!!!! Test
+      <h1 className="">Homepage</h1>
     </main>
   );
 }
