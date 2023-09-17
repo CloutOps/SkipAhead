806a4efa918ec8e250cdaab437df7bcb52a00634
Atridad Lahiji <me@atri.dad>
2023-09-16 19:05:54 -0600

    css change

--- app/vendor/page.tsx
+++ app/vendor/page.tsx
@@ -25,7 +25,7 @@ export default function Home() {
   };
 
   return (
-    <main className="flex flex-col text-center items-center justify-center gap-4">
+    <main className="flex flex-col text-center items-center justify-center px-4 py-16 gap-4">
       <h1 className="text-3xl sm:text-6xl font-bold">
         <span className="bg-gradient-to-br from-blue-600 to-green-400 bg-clip-text text-transparent box-decoration-clone">
           Welcome
