bd687e3c789e3e9843813bd41144ccb36041bcdf
bdishan <39475803+bdishan@users.noreply.github.com>
2023-09-16 23:42:03 -0600

    cosmetic change

--- app/vendor/page.tsx
+++ app/vendor/page.tsx
@@ -29,31 +29,36 @@ export default function Home() {
 
       <div className="divider"></div>
 
-      <div className="flex flex-row flex-wrap items-center justify-center gap-4">
-        <input
-          type="text"
-          name="title"
-          placeholder="Product Name"
-          onChange={(productName) => {
-            setProductName(productName.target.value);
-          }}
-        ></input>
-        <input
-          type="text"
-          name="description"
-          placeholder="Product Description"
-          onChange={(productDescription) => {
-            setProductDescription(productDescription.target.value);
-          }}
-        />
-        <input
-          type="text"
-          name="price"
-          placeholder="Product Price"
-          onChange={(productPrice) => {
-            setProductPrice(productPrice.target.value);
-          }}
-        ></input>
+      <div className="card w-96 bg-primary text-primary-content">
+        <div className="card-body">
+          <input
+            type="text"
+            name="title"
+            placeholder="Product Name"
+            onChange={(productName) => {
+              setProductName(productName.target.value);
+            }}
+            style={{ backgroundColor: '#570df8' }}
+          ></input>
+          <input
+            type="text"
+            name="description"
+            placeholder="Product Description"
+            onChange={(productDescription) => {
+              setProductDescription(productDescription.target.value);
+            }}
+            style={{ backgroundColor: '#570df8' }}
+          />
+          <input
+            type="text"
+            name="price"
+            placeholder="Product Price"
+            onChange={(productPrice) => {
+              setProductPrice(productPrice.target.value);
+            }}
+            style={{ backgroundColor: '#570df8' }}
+          ></input>
+        </div>
       </div>
 
       <button
