af2fb1c8f1c15d58a3211d0db2a2c63bf7c376d3
Atridad Lahiji <me@atri.dad>
2023-09-16 23:09:54 -0600

    Added a new environment variable (optional) to prep for the API

--- env.mjs
+++ env.mjs
@@ -3,14 +3,16 @@ import { z } from "zod";
 
 export const env = createEnv({
   server: {
-    // DATABASE_URL: z.string().url(),
     CLERK_SECRET_KEY: z.string().min(1),
   },
   client: {
     NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string().min(1),
+    NEXT_API_BASE_URL: z.string().min(1),
   },
   experimental__runtimeEnv: {
     NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY:
       process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+    NEXT_API_BASE_URL:
+      process.env.NEXT_API_BASE_URL || "http://localhost:3000/api",
   },
 });
