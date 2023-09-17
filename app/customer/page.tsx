70cfd8acc07cda6e5d6c3d271c70b19f3d1ffd6d
Saahil Rachh <60090957+SAL778@users.noreply.github.com>
2023-09-16 19:19:34 -0600

    feat: customer page

---
+++ app/customer/page.tsx
@@ -0,0 +1,17 @@
+export default function Home() {
+    return (
+      <main className="flex flex-col text-center items-center justify-center px-4 py-16 gap-4">
+        <h1 className="text-3xl sm:text-6xl font-bold">
+          <span className="bg-gradient-to-br from-blue-600 to-green-400 bg-clip-text text-transparent box-decoration-clone">
+            WELCOME,<br/>JOHN WICKS 
+          </span>
+        </h1>
+  
+        <div className="divider"></div>
+  
+        <p className="text-xl">PRODUCTS:</p>
+  
+      </main>
+    );
+  }
+  
